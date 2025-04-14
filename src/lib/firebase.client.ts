dac17df38620772097623ab8b6c5aad0fb6310bb
Marius <mariuslangeland.ml@gmail.com>
2025-04-14 15:28:30 +0200

    Fixed queueLookup to use uid

--- src/lib/firebase.client.ts
+++ src/lib/firebase.client.ts
@@ -3,7 +3,6 @@ import { initializeApp } from 'firebase/app';
 import { collection, type Firestore, getDocsFromServer, getFirestore, query, where } from 'firebase/firestore';
 import { type Auth, getAuth } from "firebase/auth";
 
-
 export const getFirebase = () => {
     const config = JSON.parse(PUBLIC_FIREBASE_CONFIG);
     const app = initializeApp(config);
@@ -16,7 +15,7 @@ export const getFirebase = () => {
 
 export const queueLookup = async () => {
     const fb = getFirebase();
-    const uid = fb.auth.currentUser;
+    const uid = fb.auth.currentUser?.uid;
 
     let col = collection(fb.firestore, 'Queue');
     let q = query(col, where('uid', '==', uid));
