326f92954c9fe7c003d4fa7e4bf0a16abb823fd5
Marius-Langeland <mariuslangeland.ml@gmail.com>
2025-04-27 01:58:56 +0200

    added redirect url

--- src/lib/supabaseClient.svelte.ts
+++ src/lib/supabaseClient.svelte.ts
@@ -1,6 +1,5 @@
 import { createClient, type Session } from "@supabase/supabase-js";
 import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
-import type { Database } from "$lib";
 
 export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY);
 
@@ -13,7 +12,10 @@ supabase.auth.onAuthStateChange(async (event, session) => {
 
 export const login = async () => {
     await supabase.auth.signInWithOAuth({
-        provider: 'discord'
+        provider: 'discord',
+        options: {
+            redirectTo: 'https://balatro-multiplayer.vercel.app/',
+        },
     });
 }
 
