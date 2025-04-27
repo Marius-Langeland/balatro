e56740d4c6de13ca95c12321d1074479c20f778e
Marius-Langeland <mariuslangeland.ml@gmail.com>
2025-04-27 02:25:57 +0200

    Added auth redirect environment variable

--- src/lib/supabaseClient.svelte.ts
+++ src/lib/supabaseClient.svelte.ts
@@ -1,5 +1,5 @@
 import { createClient, type Session } from "@supabase/supabase-js";
-import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
+import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL, PUBLIC_VERCEL_URL } from "$env/static/public";
 
 export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY);
 
@@ -10,11 +10,21 @@ supabase.auth.onAuthStateChange(async (event, session) => {
     authState.displayName = session?.user.user_metadata.custom_claims.global_name ?? '';
 });
 
+const getURL = () => {
+    let url = PUBLIC_VERCEL_URL;
+
+    // Make sure to include `https://` when not localhost.
+    url = url.startsWith('http') ? url : `https://${url}`
+    // Make sure to include a trailing `/`.
+    url = url.endsWith('/') ? url : `${url}/`
+    return url
+}
+
 export const login = async () => {
     await supabase.auth.signInWithOAuth({
         provider: 'discord',
         options: {
-            redirectTo: 'https://balatro-multiplayer.vercel.app/',
+            redirectTo: getURL(),
         },
     });
 }
